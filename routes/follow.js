564191298854ab5da1b4a274c9af881320a61804
RitaBB <ritaballesterosba@gmail.com>
2024-08-27 09:26:21 -0500

    follow_controller_completed

--- routes/follow.js
+++ routes/follow.js
@@ -1,11 +1,15 @@
-import { Router} from "express";
-
+// Importaciones
+import { Router } from "express";
 const router = Router();
-import { testFollow } from "../controllers/follow.js";
+import { testFollow, saveFollow, unfollow, following, followers } from "../controllers/follow.js";
+import { ensureAuth } from "../middlewares/auth.js";
 
-//Definir las rutas
-
+// Definir las rutas
 router.get('/test-follow', testFollow);
+router.post("/follow", ensureAuth, saveFollow);
+router.delete("/unfollow/:id", ensureAuth, unfollow);
+router.get("/following/:id?/:page?", ensureAuth, following);
+router.get("/followers/:id?/:page?", ensureAuth, followers);
 
-//Exportar el Router
+// Exportar el Router
 export default router;
